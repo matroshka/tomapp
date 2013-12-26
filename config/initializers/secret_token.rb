89a6a06ad8fc0b81e410ee6ec89d541ee7da9843
matroshka <matroshka.mint@gmail.com>
2013-12-26 02:07:56 +0100

    Validation initiale

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tomapp::Application.config.secret_key_base = 'e8743fcdc8fae610736e909b1852fb6089c52f82f53c3a44272f72e549ce89c5cb1a5adbe5ec4df5bf7f632b086a90e17df5858ec6454547defbc2836d01c9d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tomapp::Application.config.session_store :cookie_store, key: '_tomapp_session'
